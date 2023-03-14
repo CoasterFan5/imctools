8133a76601a3de4f42e18efc08f345048f37ce33
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2023-03-14 12:21:51 -0400

    Added prestige calculator

--- src/routes/prestiges/+page.svelte
+++ src/routes/prestiges/+page.svelte
@@ -1,7 +1,32 @@
+<script>
+
+	import { formatNumber } from '$lib/formatter.js';
+	let level = 0;
+	let target = 0;
+	let nextPrestigeCost = 400_000_000_000;
+	let totalCost = 400_000_000_000;
+	$: nextPrestigeCost = Math.floor(400_000_000_000 * (1.00025 ** level));
+	$: totalCost = calculateTotalCost(level, target)
+
+	let calculateTotalCost = (level, target) => {
+		let sum = 0;
+
+		for(let i = level; i < target; i++) {
+			sum += Math.floor(400_000_000_000 * (1.00025 ** i));
+		}
+
+		return sum;
+	}
+
+</script>
+
 <title>Prestige Calculator</title>
 
-<h1 class="pagetitle">Coming Soon</h1>
-<p>Blame Brock</p>
+<input placeholder="Enter Current Prestige" bind:value={level} />
+<input placeholder="Enter Target Prestige" bind:value={target} />
+
+<p>Next Prestige Cost: {formatNumber(nextPrestigeCost)}</p>
+<p>Total Cost: {formatNumber(totalCost)}</p>
 
 <style>
 	.pagetitle {
